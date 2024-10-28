7cd04fdebdd277a0458a1128a25b209861b3aee4
darkelvesasd <96123329+darkelvesasd@users.noreply.github.com>
2024-10-28 11:38:27 +0800

    Create 186. 弹簧板.cpp

---
+++ 186. 弹簧板.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	int cnt=0;
+	int n;
+	cin>>n;
+	int i=0;
+	vector<int>(n);
+	while(cin>>v[i++]);
+	for(int i=0;i<n;)
+	{
+		i+=v[i];
+		cnt++;
+	}
+	cout<< cnt;
+	return 0;
+}
